e1e09fb04960a884e64d610679bd67757abee1d1
adrhc <adrhc@outlook.com>
2020-10-29 19:30:31 +0200

    main.js: using 8 items

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Test to see if the browser supports the HTML template element by checking
 // for the presence of the template element's content attribute.
 if ('content' in document.createElement('template')) {
-    const DATA = Array(5).fill()
+    const DATA = Array(8).fill()
         .map((_, i) => {
             return {nr: i + 1, code: Math.random(), name: `Stuff ${i}`};
         });
