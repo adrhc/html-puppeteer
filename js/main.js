6ae396e72c29eaf7695e83df7150b1860ef1b048
adrhc <adrhc@outlook.com>
2020-11-04 10:05:15 +0200

    main.js: using $(document).ready() + table-data.json

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // Test to see if the browser supports the HTML template element by checking
 // for the presence of the template element's content attribute.
-if ('content' in document.createElement('template')) {
-    const DATA = Array(8).fill()
-        .map((_, i) => {
-            return {nr: i + 1, code: Math.random(), name: `Stuff ${i}`};
-        });
-    new TabularEditor(DATA, "producttable",
-        "readOnlyRowTmpl", "editorRowTmpl").render();
-} else {
-    // Find another way to add the rows to the table because
-    // the HTML template element is not supported.
-}
+$(document).ready(() => {
+    if ('content' in document.createElement('template')) {
+        const DATA = Array(8).fill()
+            .map((_, i) => {
+                return {nr: i + 1, code: Math.random(), name: `Stuff ${i}`};
+            });
+        console.log(JSON.stringify(DATA));
+        new TabularEditor(DATA, "producttable",
+            "readOnlyRowTmpl", "editorRowTmpl").render();
+    } else {
+        // Find another way to add the rows to the table because
+        // the HTML template element is not supported.
+    }
+})
