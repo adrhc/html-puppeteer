840a6aa027bc892f5e446f1d8b9e483b608df4a7
adrhc <adrhc@outlook.com>
2020-12-09 14:52:40 +0200

    + libraries/handlebars index.html: readOnlyRowTmpl mustache-table-elem-adapter.js: _renderTemplate, _renderBodyTemplate

--- js/adrhc/go/ro/jquery-widgets/adapter/mustache-table-elem-adapter.js
+++ js/adrhc/go/ro/jquery-widgets/adapter/mustache-table-elem-adapter.js
@@ -11,22 +11,36 @@ class MustacheTableElemAdapter extends TableElementAdapter {
     }
 
     renderBody(data) {
-        const html = Mustache.render(this.bodyTmplHtml, data, {bodyRowTmpl: this.bodyRowTmplHtml})
+        const html = this._renderBodyTemplate(data);
         super.$tbody.html(html);
     }
 
     renderRowAtIndex(rowIndex, rowTmplHtml, cellsView, replaceExistingIfExists) {
-        const rowHtml = cellsView ? Mustache.render(rowTmplHtml, cellsView) : rowTmplHtml;
+        const rowHtml = this._renderTemplate(rowTmplHtml, cellsView);
         super.renderRowAtIndex(rowIndex, rowHtml, replaceExistingIfExists);
     }
 
     renderRowBeforeDataId(rowDataId, rowTmplHtml, cellsView, replaceExistingIfExists) {
-        const rowHtml = cellsView ? Mustache.render(rowTmplHtml, cellsView) : rowTmplHtml;
+        const rowHtml = this._renderTemplate(rowTmplHtml, cellsView);
         super.renderRowBeforeDataId(rowDataId, rowHtml, replaceExistingIfExists);
     }
 
     renderRowAfterDataId(rowDataId, rowTmplHtml, cellsView, replaceExistingIfExists) {
-        const rowHtml = cellsView ? Mustache.render(rowTmplHtml, cellsView) : rowTmplHtml;
+        const rowHtml = this._renderTemplate(rowTmplHtml, cellsView);
         super.renderRowAfterDataId(rowDataId, rowHtml, replaceExistingIfExists);
+    }
+
+    _renderBodyTemplate(data) {
+        return Mustache.render(this.bodyTmplHtml, data, {bodyRowTmpl: this.bodyRowTmplHtml})
+    }
+
+    _renderTemplate(rowTmplHtml, cellsView) {
+        // return cellsView ? Mustache.render(rowTmplHtml, cellsView) : rowTmplHtml;
+        if (cellsView) {
+            const template = Handlebars.compile(rowTmplHtml);
+            return template(cellsView);
+        } else {
+            return rowTmplHtml;
+        }
     }
 }
