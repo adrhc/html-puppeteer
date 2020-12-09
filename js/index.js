3a238b3bbad9b50648394c01bf2c58097f198826
adrhc <adrhc@outlook.com>
2020-12-09 15:40:23 +0200

    index.js: fix test data

--- js/index.js
+++ js/index.js
@@ -12,10 +12,10 @@ if (Modernizr.template) {
         const persons = {
             "1": new Person(1, "gigi1", "kent1",
                 {"1": {id: 1, name: "cat1"}, "2": {id: 2, name: "cat2"}, "3": {id: 3, name: "cat3"}}),
-            "2": new Person(2, "gigi1", "kent1",
-                {"21": {id: 1, name: "cat1"}, "22": {id: 2, name: "cat2"}, "23": {id: 3, name: "cat3"}}),
-            "3": new Person(3, "gigi1", "kent1",
-                {"31": {id: 1, name: "cat1"}, "32": {id: 2, name: "cat2"}, "33": {id: 3, name: "cat3"}})
+            "2": new Person(2, "gigi2", "kent2",
+                {"21": {id: 21, name: "cat21"}, "22": {id: 22, name: "cat22"}, "23": {id: 23, name: "cat23"}}),
+            "3": new Person(3, "gigi3", "kent3",
+                {"31": {id: 31, name: "cat31"}, "32": {id: 32, name: "cat32"}, "33": {id: 33, name: "cat33"}})
         };
         const personsRepository = new InMemoryPersonsRepository(persons);
 
