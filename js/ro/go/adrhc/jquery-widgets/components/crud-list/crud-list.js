860e169d65858a842c0f91c75fdb5ee29b3b01c5
adrhc <adrhc@outlook.com>
2020-12-14 23:38:11 +0200

    crud-list.js: DRAFT

--- js/ro/go/adrhc/jquery-widgets/components/crud-list/crud-list.js
+++ js/ro/go/adrhc/jquery-widgets/components/crud-list/crud-list.js
@@ -1,3 +1,13 @@
 class CrudListComponent extends SimpleListComponent {
+    /**
+     * @param mustacheTableElemAdapter {MustacheTableElemAdapter}
+     * @param repository {CrudRepository}
+     * @param state {CrudListState}
+     * @param view {SimpleListView}
+     */
+    constructor(mustacheTableElemAdapter,
+                repository, state, view) {
+        super(mustacheTableElemAdapter, repository, state, view);
+    }
 
 }
