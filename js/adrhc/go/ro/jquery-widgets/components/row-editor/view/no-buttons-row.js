57ac043e28292fc874d027547397d2e4db7ab560
adrhc <adrhc@outlook.com>
2020-12-19 01:52:23 +0200

    adrhc paths refactor + db-cats-editable-list.html

--- js/adrhc/go/ro/jquery-widgets/components/row-editor/view/no-buttons-row.js
+++
@@ -1,7 +0,0 @@
-class NoButtonsRow {
-    hide() {
-    }
-
-    show(item) {
-    }
-}
--- js/components/person-row-editor-old/factories/person-row-editor-old-factory.js
+++
@@ -1,8 +0,0 @@
-class PersonRowEditorOldFactory {
-    create(mustacheTableElemAdapter) {
-        const editableRow = new EditableRow(mustacheTableElemAdapter, {rowTmplId: "personsEditableRowTmpl"});
-        const buttonsRow = new ButtonsRow(mustacheTableElemAdapter, {});
-        const rowEditorView = new RowEditorView(editableRow, buttonsRow);
-        return new PersonRowEditorOldComponent(rowEditorView);
-    }
-}
