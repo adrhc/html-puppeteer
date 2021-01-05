8e56c58d3569f683f472789dca026d6508091b6b
adrhc <adrhc@outlook.com>
2021-01-05 22:00:45 +0200

    RestUtils.unwrapHAL: check null object parameter

--- js/ro/go/adrhc/jquery-widgets/utils/rest-utils.js
+++ js/ro/go/adrhc/jquery-widgets/utils/rest-utils.js
@@ -1,6 +1,11 @@
 class RestUtils {
     unwrapHAL(object) {
-        if (object.hasOwnProperty("_embedded")) {
+        if (!object) {
+            return object;
+        }
+        if ($.isArray(object)) {
+            object.forEach(it => RestUtils.prototype.unwrapHAL(it));
+        } else if (object.hasOwnProperty && object.hasOwnProperty("_embedded")) {
             object = object._embedded;
             for (let p in object) {
                 if (object.hasOwnProperty(p)) {
@@ -8,8 +13,6 @@ class RestUtils {
                 }
             }
             return RestUtils.prototype.unwrapHAL(object);
-        } else if ($.isArray(object)) {
-            object.forEach(it => RestUtils.prototype.unwrapHAL(it));
         } else {
             delete object._links;
         }
