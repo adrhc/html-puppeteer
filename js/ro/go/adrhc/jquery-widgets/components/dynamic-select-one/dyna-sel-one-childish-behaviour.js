53107e47b1e9c71a9e500c0a2bb0ea36ed049d1d
adrhc <adrhc@outlook.com>
2021-01-26 19:11:07 +0200

    DynaSelOneChildishBehaviour: childStateProperty -> childCompToEntityConverter, added parentSectionToChildEntityConverter

--- js/ro/go/adrhc/jquery-widgets/components/dynamic-select-one/dyna-sel-one-childish-behaviour.js
+++ js/ro/go/adrhc/jquery-widgets/components/dynamic-select-one/dyna-sel-one-childish-behaviour.js
@@ -2,11 +2,14 @@ class DynaSelOneChildishBehaviour extends DefaultChildishBehaviour {
     /**
      * @param parentComp {AbstractComponent}
      * @param childStateProperty {string} is the parentState property where to save the selectedItem
-     * @param componentStateToEntityConverter {function({}): IdentifiableEntity}
+     * @param childCompToEntityConverter {function({}): IdentifiableEntity} converts child-component state-snapshot to child entity
+     * @param parentSectionToChildEntityConverter {function({}): IdentifiableEntity} converts the portion from parent that belongs to the child to child entity
      */
-    constructor(parentComp, childStateProperty, componentStateToEntityConverter) {
+    constructor(parentComp, childStateProperty, childCompToEntityConverter,
+                parentSectionToChildEntityConverter = childCompToEntityConverter) {
         super(parentComp, childStateProperty);
-        this.componentStateToEntityConverter = componentStateToEntityConverter;
+        this.childCompToEntityConverter = childCompToEntityConverter;
+        this.parentSectionToChildEntityConverter = parentSectionToChildEntityConverter;
     }
 
     /**
@@ -18,33 +21,25 @@ class DynaSelOneChildishBehaviour extends DefaultChildishBehaviour {
      */
     copyChildState(parentState, useOwnerOnFields) {
         const selectedItem = this._childComp.state.currentState;
-        parentState[this.childStateProperty] = this.componentStateToEntityConverter(selectedItem);
+        parentState[this.childStateProperty] = this.childCompToEntityConverter(selectedItem);
     }
 
     /**
      * I assume that parentComp will already be set!
-     *
+     *ddd
      * this.parentComp must be {IdentifiableRowComponent}
      *
      * @param childComp {DynamicSelectOneComponent}
      */
     set childComp(childComp) {
-        this._updateWithDynaSelOneItem(childComp);
         this._childComp = childComp;
-    }
-
-    /**
-     * @param childComp {DynamicSelectOneComponent}
-     * @protected
-     */
-    _updateWithDynaSelOneItem(childComp) {
         const parentState = this.parentComp.state.currentState;
         const dynaSelOneItem = this.extractChildState(parentState);
         childComp.dynaSelOneState.updateWithDynaSelOneItem(dynaSelOneItem);
     }
 
     extractChildState(parentState) {
-        const identifiableEntity = super.extractChildState(parentState);
-        return this.componentStateToEntityConverter(identifiableEntity);
+        const childStateFromParent = super.extractChildState(parentState);
+        return this.parentSectionToChildEntityConverter(childStateFromParent);
     }
 }
