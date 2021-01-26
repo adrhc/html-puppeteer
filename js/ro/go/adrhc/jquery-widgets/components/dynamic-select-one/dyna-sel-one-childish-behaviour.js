502ec23d9236233edb76c40034f28fd1c20fc7e6
adrhc <adrhc@outlook.com>
2021-01-26 18:56:03 +0200

    DynaSelOneChildishBehaviour: childEntityConverter -> componentStateToEntityConverter

--- js/ro/go/adrhc/jquery-widgets/components/dynamic-select-one/dyna-sel-one-childish-behaviour.js
+++ js/ro/go/adrhc/jquery-widgets/components/dynamic-select-one/dyna-sel-one-childish-behaviour.js
@@ -2,22 +2,23 @@ class DynaSelOneChildishBehaviour extends DefaultChildishBehaviour {
     /**
      * @param parentComp {AbstractComponent}
      * @param childStateProperty {string} is the parentState property where to save the selectedItem
-     * @param childEntityConverter {function({}): IdentifiableEntity}
+     * @param componentStateToEntityConverter {function({}): IdentifiableEntity}
      */
-    constructor(parentComp, childStateProperty, childEntityConverter) {
+    constructor(parentComp, childStateProperty, componentStateToEntityConverter) {
         super(parentComp, childStateProperty);
-        this.childEntityConverter = childEntityConverter;
+        this.componentStateToEntityConverter = componentStateToEntityConverter;
     }
 
     /**
-     * When having kids and useOwnerOnFields is null than the owner is used otherwise useOwnerOnFields is considered.
+     * When having kids and useOwnerOnFields is null than the owner is used despite
+     * useOwnerOnFields value otherwise the useOwnerOnFields value is considered.
      *
      * @param parentState
      * @param [useOwnerOnFields] {boolean}
      */
     copyChildState(parentState, useOwnerOnFields) {
         const selectedItem = this._childComp.state.currentState;
-        parentState[this.childStateProperty] = this.childEntityConverter(selectedItem);
+        parentState[this.childStateProperty] = this.componentStateToEntityConverter(selectedItem);
     }
 
     /**
@@ -44,6 +45,6 @@ class DynaSelOneChildishBehaviour extends DefaultChildishBehaviour {
 
     extractChildState(parentState) {
         const identifiableEntity = super.extractChildState(parentState);
-        return this.childEntityConverter(identifiableEntity);
+        return this.componentStateToEntityConverter(identifiableEntity);
     }
 }
