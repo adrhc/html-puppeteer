443bb97acc3548f305a51953dd48fc380a8ea41e
adrhc <adrhc@outlook.com>
2021-01-26 18:39:29 +0200

    SimpleListFactory: added bodyRowTmplHtml

--- js/ro/go/adrhc/jquery-widgets/components/simple-list/factories/simple-list-factory.js
+++ js/ro/go/adrhc/jquery-widgets/components/simple-list/factories/simple-list-factory.js
@@ -1,19 +1,21 @@
 class SimpleListFactory {
     /**
-     * @param items {IdentifiableEntity[]}
-     * @param tableIdOrJQuery {string|jQuery<HTMLTableElement>}
-     * @param bodyRowTmplId {string|undefined} could be empty when not using a row template (but only a table-rows template)
-     * @param mustacheTableElemAdapter {MustacheTableElemAdapter}
-     * @param repository {CrudRepository}
-     * @param state {SimpleListState}
-     * @param view {SimpleListView}
+     * @param [items] {IdentifiableEntity[]}
+     * @param [tableIdOrJQuery] {string|jQuery<HTMLTableElement>}
+     * @param [bodyRowTmplId] {string} could be empty when not using a row template (but only the table)
+     * @param [bodyRowTmplHtml] {string}
+     * @param [mustacheTableElemAdapter] {MustacheTableElemAdapter}
+     * @param [repository] {CrudRepository}
+     * @param [state] {SimpleListState}
+     * @param [view] {SimpleListView}
      * @return {SimpleListComponent}
      */
     static create({
                       items = [],
                       tableIdOrJQuery,
                       bodyRowTmplId,
-                      mustacheTableElemAdapter = new MustacheTableElemAdapter(tableIdOrJQuery, bodyRowTmplId),
+                      bodyRowTmplHtml,
+                      mustacheTableElemAdapter = new MustacheTableElemAdapter(tableIdOrJQuery, bodyRowTmplId, bodyRowTmplHtml),
                       repository = new InMemoryCrudRepository(items),
                       state = new SimpleListState(),
                       view = new SimpleListView(mustacheTableElemAdapter)
