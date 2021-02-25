b5350ccbaeabc57fcfc769553da73698eef4651a
adrhc <adrhc@outlook.com>
2021-02-25 22:04:17 +0200

    16container-component-dyna.js: adding a new dog every {{seconds}} seconds

--- js/16container-component-dyna.js
+++ js/16container-component-dyna.js
@@ -1,31 +1,28 @@
-if (Modernizr.template) {
-    $.ajaxSetup({
-        contentType: 'application/json',
-        processData: false
-    });
-    $.ajaxPrefilter(function (options) {
-        if (options.contentType === 'application/json' && options.data) {
-            options.data = JSON.stringify(options.data);
-        }
-    });
-
-    $(() => {
-        // const comp = new ContainerComponent($("[data-jqw-type='ContainerComponent']"));
-        const comp = JqueryWidgetsUtil.autoCreate();
-        comp.compositeBehaviour.addChildComponentFactory([(parentComp) => {
-            const tableIdOrJQuery = $("#dogsTable", parentComp.view.$elem);
-            return SimpleListFactory.create({items: DbMock.DOGS, tableIdOrJQuery, childStateProperty: "dogs"});
-        }, (parentComp) => {
-            const elemIdOrJQuery = $("#dyna-sel-one", parentComp.view.$elem);
-            return DynamicSelectOneFactory.create(elemIdOrJQuery, DbMock.DYNA_SEL_ONE_PERS_REPOSITORY, {});
-        }]);
-        comp.init()
-            .then(() => comp.processStateChange(new StateChange("UPDATE_ALL", {
-                dogs: [{id: 4, name: "dog4"}, ...DbMock.DOGS]
-            })))
-            .then(() => console.log("16container-component-dyna.js started"));
-    });
-} else {
-    // Find another way to add the rows to the table because
-    // the HTML template element is not supported.
+function findDogsListComp(comp) {
+    return comp.compositeBehaviour.findKids(
+        (kid) => kid instanceof SimpleListComponent).pop();
 }
+
+function procStateChgFnOf(comp) {
+    return () => {
+        const dogs = findDogsListComp(comp).repository.items;
+        dogs.push({id: dogs.length + 1, name: `dog${dogs.length + 1}`});
+        const updateAllStateChange = new StateChange("UPDATE_ALL", {dogs});
+        return comp.processStateChange(updateAllStateChange);
+    };
+}
+
+$(() => {
+    // const comp = new ContainerComponent($("[data-jqw-type='ContainerComponent']"));
+    const comp = JqueryWidgetsUtil.autoCreate();
+    comp.compositeBehaviour.addChildComponentFactory([(parentComp) => {
+        const tableIdOrJQuery = $("#dogsTable", parentComp.view.$elem);
+        return SimpleListFactory.create({items: DbMock.DOGS, tableIdOrJQuery, childStateProperty: "dogs"});
+    }, (parentComp) => {
+        const elemIdOrJQuery = $("#dyna-sel-one", parentComp.view.$elem);
+        return DynamicSelectOneFactory.create(elemIdOrJQuery, DbMock.DYNA_SEL_ONE_PERS_REPOSITORY, {});
+    }]);
+    comp.init()
+        .then(() => setInterval(procStateChgFnOf(comp), comp.state.currentState.seconds * 1000))
+        .then(() => console.log("16container-component-dyna.js started"));
+});
