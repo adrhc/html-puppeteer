6b216e10b48d4007bb1b2e51d714e78f273760f1
adrhc <adrhc@outlook.com>
2021-02-25 19:19:00 +0200

    + ContainerComponent DrawingComponent extends ContainerComponent

--- js/ro/go/adrhc/jquery-widgets/components/drawing/DrawingComponent.js
+++ js/ro/go/adrhc/jquery-widgets/components/drawing/DrawingComponent.js
@@ -1,46 +1,6 @@
-class DrawingComponent extends AbstractComponent {
-    /**
-     * @param {string|jQuery<HTMLElement} elemIdOrJQuery
-     * @param {BasicState} [state]
-     * @param {DefaultTemplatingView} [view]
-     * @param {ComponentConfiguration} [config]
-     */
-    constructor(elemIdOrJQuery,
-                config = $.extend(new ComponentConfiguration(),
-                    {clearChildrenOnReset: true}, DomUtils.jQueryOf(elemIdOrJQuery).data()),
-                state = new BasicState(config),
-                view = new DefaultTemplatingView(elemIdOrJQuery, config)) {
-        super(state, view, config);
-        if (config.dontAutoInitialize) {
-            return;
-        }
-        return super.init().then(() => this);
-    }
-
+class DrawingComponent extends ContainerComponent {
     processStateChange(stateChangeOrJustData, dontRecordStateEvents) {
         super.reset();
         return super.processStateChange(stateChangeOrJustData, dontRecordStateEvents);
-    }
-
-    _reloadState() {
-        if (this.config.dontReloadFromState) {
-            return super._reloadState();
-        } else {
-            return this._stateChangePromiseFromState();
-        }
-    }
-
-    /**
-     * @return {Promise<StateChange>}
-     * @protected
-     */
-    _stateChangePromiseFromState() {
-        return new Promise((resolve) => {
-            console.debug(`${this.constructor.name}._stateChangePromiseFromState`);
-            console.debug(JSON.stringify(this.state.currentState));
-            const stateChange = new StateChange(this.config.stateChangeRequest, this.state.currentState);
-            this.state.collectStateChange(stateChange);
-            setTimeout(() => resolve(stateChange))
-        })
     }
 }
