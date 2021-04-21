8591eb44f9e762f9611a99493ff3686d4827cbb7
adrhc <adrhc@outlook.com>
2021-04-21 19:35:35 +0300

    6.1create-delete-list-and-dyna-sel-one.js working DynamicSelectOneFactory: removed all methods

--- js/6.1create-delete-list-and-dyna-sel-one.js
+++ js/6.1create-delete-list-and-dyna-sel-one.js
@@ -1,34 +1,20 @@
+const JSON_RESULT = "[{\"id\":\"0\",\"name\":\"dog 0\",\"person\":{\"id\":1,\"firstName\":\"gigi1\",\"lastName\":\"kent1\",\"friend\":{\"id\":2,\"firstName\":\"gigi2\",\"lastName\":\"kent2\",\"cats\":[]},\"cats\":[{\"id\":1,\"name\":\"cat1\",\"person\":{\"id\":1,\"firstName\":\"gigi1\",\"lastName\":\"kent1\",\"cats\":[]},\"friendId\":1},{\"id\":2,\"name\":\"cat2\",\"person\":{\"id\":1,\"firstName\":\"gigi1\",\"lastName\":\"kent1\",\"cats\":[]},\"friendId\":1},{\"id\":3,\"name\":\"cat3\",\"person\":{\"id\":1,\"firstName\":\"gigi1\",\"lastName\":\"kent1\",\"cats\":[]},\"friendId\":1}]}},{\"id\":\"1\",\"name\":\"dog 1\"},{\"id\":\"3\",\"name\":\"updated dog3\"},{\"id\":\"4\",\"name\":\"dog 4\"},{\"name\":\"new dog\"},{\"id\":\"2\",\"name\":\"restored dog2 with prepend\"}]";
+
 $(() => {
-    const DOGS = DbMocks.DOGS;
+    const DOGS = DbMocks.dogsOf(5);
     _.defaults(DOGS[0], {person: DbMocks.PERSONS_REPOSITORY.getById(1, true)});
 
     // by default on creation the row is prepended to table
-    const newItemsGoLast = true;
-
-    // DynamicSelectOneComponent child component factory (see ChildComponentFactory)
-    const dynaSelOneCompFactory = {
-        /**
-         * @param idRowCompParent {IdentifiableRowComponent}
-         * @return {DynamicSelectOneComponent}
-         */
-        createChildComponent: (idRowCompParent) => {
-            const $parentElem = idRowCompParent.view.$elem;
-            AssertionUtils.isTrue($parentElem.length === 1, "dynaSelOneCompFactory, DynamicSelectOneFactory");
-
-            return DynamicSelectOneFactory.create(
-                $("[data-id='dyna-sel-one']", idRowCompParent.view.$elem),
-                DbMocks.DYNA_SEL_ONE_PERS_REPOSITORY, {
-                    childishBehaviour: new DynaSelOneOnRowChildishBehaviour(idRowCompParent, "person", Person.parse)
-                });
-        }
-    };
+    const rowPositionOnCreate = "prepend";
 
     // dogs table
-    const createDeleteList = CreateDeleteListFactory.create("dogsTable", {
+    const createDeleteList = new CreateDeleteListComponent({
+        elemIdOrJQuery: "dogsTable",
         items: DOGS,
-        newItemsGoLast,
-        bodyRowTmplId: "dogsTableRowTmpl",
-        rowChildCompFactories: dynaSelOneCompFactory
+        rowChildCompFactories: new DynaSelOneRowChildCompFactory(
+            "person", Person.parse, DbMocks.DYNA_SEL_ONE_PERS_REPOSITORY),
+        dontAutoInitialize: true,
+        rowPositionOnCreate
     });
 
     // some doWithState operations on createDeleteList
@@ -47,10 +33,14 @@ $(() => {
             crudListState.removeById(2);
             crudListState.insertItem({
                 id: 2,
-                name: `restored dog2 with ${newItemsGoLast ? "append" : "prepend"}`
+                name: `restored dog2 with ${rowPositionOnCreate}`
             });
         }))
         // showing the entire table extracted data
-        .then(() => console.log("ElasticListComponent.extractAllEntities:\n",
-            createDeleteList.extractAllEntities()));
+        .then(() => {
+            const entities = createDeleteList.extractAllEntities();
+            console.log("ElasticListComponent.extractAllEntities:\n", entities);
+            AssertionUtils.isTrue(entities.length === 6, "entities.length === 4");
+            AssertionUtils.isTrue(JSON.stringify(entities) === JSON_RESULT, "JSON doesn't match!")
+        });
 });
