f89410ecb95ef471e98b3468a3cb1778874c13ce
Adrian Petre <adrian.petre@endava.com>
2021-08-25 20:35:38 +0300

    DRAFT 7.1editable-list-state-changes.js

--- js/7.1editable-list-state-changes.js
+++ js/7.1editable-list-state-changes.js
@@ -13,15 +13,9 @@ if (Modernizr.template) {
         // dogs table with both read-only and editable row
         const elemIdOrJQuery = "dogsTable";
 
-        const repository = new InMemoryCrudRepository(DbMocks.DOGS, undefined, (it) => {
-            it.id = Math.abs(it.id);
-            return it;
-        });
-
         const component = new EditableListComponent({
             elemIdOrJQuery,
             items: DbMocks.DOGS,
-            repository,
             bodyRowTmplId: "dogsTableRowTmpl",
             offRow: new IdentifiableRowComponent({
                 elemIdOrJQuery,
