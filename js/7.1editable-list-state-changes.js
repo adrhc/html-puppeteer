e7b7bb999c553dfbf6b711a9597e41b30278e62e
Adrian Petre <adrian.petre@endava.com>
2021-08-27 18:14:38 +0300

    7.1editable-list-state-changes.js createNewItem with id = 11 otherwise add button won't work

--- js/7.1editable-list-state-changes.js
+++ js/7.1editable-list-state-changes.js
@@ -48,6 +48,7 @@ $(() => {
                 name: `changed (NOT IN REPOSITORY YET, lost on reload) dog5 position/index using append`
             }, {append: true});
             editableListState.createNewItem({
+                id: 11,
                 name: `created (NOT IN REPOSITORY YET, lost on reload) dog11 position/index using default`
             });
         }))
@@ -55,9 +56,9 @@ $(() => {
             const entities = component.extractAllEntities();
             console.log("SelectableListComponent.extractAllEntities:\n", entities);
             AssertionUtils.isTrue(entities.length === 11, "entities.length must be 11!");
-            AssertionUtils.isTrue(JSON.stringify(entities) === "[{},{\"id\":\"4\"},{\"id\":\"0\"},{\"id\":\"1\"},{\"id\":\"3\"},{\"id\":\"6\"},{\"id\":\"7\"},{\"id\":\"8\"},{\"id\":\"9\"},{\"id\":\"2\"},{\"id\":\"5\"}]",
+            AssertionUtils.isTrue(JSON.stringify(entities) === "[{\"id\":\"11\"},{\"id\":\"4\"},{\"id\":\"0\"},{\"id\":\"1\"},{\"id\":\"3\"},{\"id\":\"6\"},{\"id\":\"7\"},{\"id\":\"8\"},{\"id\":\"9\"},{\"id\":\"2\"},{\"id\":\"5\"}]",
                 "Extracted entities didn't match!");
-            AssertionUtils.isTrue(JSON.stringify(component.state.currentState) === "[{\"id\":\"newId\",\"name\":\"created (NOT IN REPOSITORY YET, lost on reload) dog11 position/index using default\"},{\"id\":4,\"name\":\"changed (NOT IN REPOSITORY YET, lost on reload) dog4 position/index using prepend\"},{\"id\":0,\"name\":\"dog 0\"},{\"id\":1,\"name\":\"dog 1\"},{\"id\":3,\"name\":\"updated (NOT IN REPOSITORY YET, lost on reload) dog3 (position/index not changed)\"},{\"id\":6,\"name\":\"dog 6\"},{\"id\":7,\"name\":\"dog 7\"},{\"id\":8,\"name\":\"dog 8\"},{\"id\":9,\"name\":\"dog 9\"},{\"id\":2,\"name\":\"restored (NOT IN REPOSITORY YET, lost on reload) dog2 with position/index changed using append\"},{\"id\":5,\"name\":\"changed (NOT IN REPOSITORY YET, lost on reload) dog5 position/index using append\"}]",
+            AssertionUtils.isTrue(JSON.stringify(component.state.currentState) === "[{\"id\":11,\"name\":\"created (NOT IN REPOSITORY YET, lost on reload) dog11 position/index using default\"},{\"id\":4,\"name\":\"changed (NOT IN REPOSITORY YET, lost on reload) dog4 position/index using prepend\"},{\"id\":0,\"name\":\"dog 0\"},{\"id\":1,\"name\":\"dog 1\"},{\"id\":3,\"name\":\"updated (NOT IN REPOSITORY YET, lost on reload) dog3 (position/index not changed)\"},{\"id\":6,\"name\":\"dog 6\"},{\"id\":7,\"name\":\"dog 7\"},{\"id\":8,\"name\":\"dog 8\"},{\"id\":9,\"name\":\"dog 9\"},{\"id\":2,\"name\":\"restored (NOT IN REPOSITORY YET, lost on reload) dog2 with position/index changed using append\"},{\"id\":5,\"name\":\"changed (NOT IN REPOSITORY YET, lost on reload) dog5 position/index using append\"}]",
                 "state didn't match!")
             console.log("ASSERTIONS PASSED!");
         });
