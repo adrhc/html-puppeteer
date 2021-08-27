bb0e82b497828c46d518f7a83573c7f1862233bf
Adrian Petre <adrian.petre@endava.com>
2021-08-27 18:06:05 +0300

    7.1editable-list-state-changes.js assertions 7editable-list.js using JQWUtil.createComponents() only

--- js/7editable-list.js
+++ js/7editable-list.js
@@ -1,34 +1,3 @@
 $(() => {
-    const elemIdOrJQuery = "dogsTable";
-
-    const component = new EditableListComponent({
-        elemIdOrJQuery,
-        dontAutoInitialize: true
-    });
-
-    return component
-        .init()
-        .then(() => component.doWithState((state) => {
-            /**
-             * @type {EditableListState}
-             */
-            const editableListState = state;
-            editableListState.removeById(2);
-            editableListState.insertItem({
-                id: 2,
-                name: `restored (NOT IN REPOSITORY YET, lost on reload) dog2 with position/index changed using append`
-            }, {append: true});
-            editableListState.updateItem({
-                id: 3,
-                name: "updated (NOT IN REPOSITORY YET, lost on reload) dog3 (position/index not changed)"
-            });
-            editableListState.updateItem({
-                id: 4,
-                name: `changed (NOT IN REPOSITORY YET, lost on reload) dog4 position/index using prepend`
-            }, {append: false});
-            editableListState.updateItem({
-                id: 5,
-                name: `changed (NOT IN REPOSITORY YET, lost on reload) dog5 position/index using append`
-            }, {append: true});
-        }));
+    JQWUtil.createComponents();
 });
