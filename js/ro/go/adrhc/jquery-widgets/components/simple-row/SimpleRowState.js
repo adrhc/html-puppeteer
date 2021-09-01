4e0f58d8debd9814eb2d6d286320393e35c8f2e1
Adrian Petre <adrian.petre@endava.com>
2021-09-01 22:29:45 +0300

    DRAFT of*() to constructors and CrudListState.js type check + SimpleRowState.js

---
+++ js/ro/go/adrhc/jquery-widgets/components/simple-row/SimpleRowState.js
@@ -0,0 +1,37 @@
+/**
+ * @typedef {EntityRow} T
+ * @extends {TaggingStateHolder<EntityRow>}
+ */
+class SimpleRowState extends TaggingStateHolder {
+    /**
+     * @param {T=} previousEntityRow
+     * @param {T=} newEntityRow
+     * @return {StateChange<T>[]}
+     * @protected
+     */
+    _stateChangesOf(previousEntityRow, newEntityRow) {
+        if (newEntityRow == null) {
+            return new StateChange(previousEntityRow);
+        }
+        const previousRowId = previousEntityRow?.entity?.id;
+        const positionChanged = newEntityRow?.index !== previousEntityRow?.index;
+        const changes = [];
+        // previous row exists and position changed
+        if (previousRowId != null && positionChanged) {
+            // removing the previous row because it has to be redrawn
+            // anyway considering that at least its position changed
+            changes.push(new StateChange(previousEntityRow));
+        }
+        // previous row is missing or the position changed
+        if (previousRowId == null || positionChanged) {
+            AssertionUtils.isTrue(PositionUtils.areAllButIndexValid(newEntityRow),
+                `The relative positioning values must be provided!\n${JSON.stringify(newEntityRow)}`);
+            changes.push(new StateChange(undefined, newEntityRow));
+        }
+        // previous row exists and the position is the same
+        if (previousRowId != null && !positionChanged) {
+            changes.push(new StateChange(previousEntityRow, newEntityRow));
+        }
+        return changes;
+    }
+}
--- js/ro/go/adrhc/jquery-widgets/components/state/StateHolder.js
+++ js/ro/go/adrhc/jquery-widgets/components/state/StateHolder.js
@@ -3,67 +3,6 @@
  */
 class StateHolder {
     /**
-     * @param {{initialState: undefined|T, stateChangeMapper: undefined|IdentityStateChangeMapper<T|T>, changesCollector: undefined|StateChangesCollector<T|T>=}} [options]
-     */
-    constructor({initialState, stateChangeMapper, changesCollector} = {}) {
-        this._currentState = initialState;
-        this._stateChanges = changesCollector ?? new StateChangesCollector(
-            stateChangeMapper ?? new IdentityStateChangeMapper());
-    }
-
-    /**
-     * @param {T=} newState
-     * @param {boolean=} dontRecordStateEvents
-     * @return {StateChange<T>|boolean} the newly created StateChange or, if dontRecordStateEvents = true, whether a state change occurred
-     */
-    replace(newState, dontRecordStateEvents) {
-        if (this._currentStateEquals(newState)) {
-            return false;
-        }
-
-        const previousState = this._replaceImpl(newState);
-
-        if (dontRecordStateEvents) {
-            return true;
-        }
-
-        const stateChange = new StateChange(previousState, newState);
-        return this.collectStateChange(stateChange);
-    }
-
-    /**
-     * @param {T} [state] is the new state value to store
-     * @return {T} the previous state
-     * @protected
-     */
-    _replaceImpl(state) {
-        const previousState = this._currentState;
-        this._currentState = state;
-        return previousState;
-    }
-
-    /**
-     * @param {T|T=} anotherState
-     * @return {boolean}
-     * @protected
-     */
-    _currentStateEquals(anotherState) {
-        return this._currentState == null && anotherState == null || this._currentState === anotherState;
-    }
-
-    /**
-     * @return {StateChange<T|T>|undefined}
-     */
-    collectStateChange(stateChange) {
-        return this._stateChanges.collect(stateChange);
-    }
-
-    reset() {
-        this._stateChanges.reset();
-        this._currentState = undefined;
-    }
-
-    /**
      * @type {StateChangesCollector<T|T>}
      * @protected
      */
@@ -94,5 +33,84 @@ class StateHolder {
      */
     set currentState(currentState) {
         this._currentState = currentState;
+    }
+
+    /**
+     * @param {{initialState: undefined|T, stateChangeMapper: undefined|IdentityStateChangeMapper<T|T>, changesCollector: undefined|StateChangesCollector<T|T>=}} [options]
+     */
+    constructor({initialState, stateChangeMapper, changesCollector} = {}) {
+        this._currentState = initialState;
+        this._stateChanges = changesCollector ?? new StateChangesCollector(
+            stateChangeMapper ?? new IdentityStateChangeMapper());
+    }
+
+    /**
+     * @param {T=} newState
+     * @param {boolean=} dontRecordStateEvents
+     * @return {StateChange<T>|boolean} the newly created StateChange or, if dontRecordStateEvents = true, whether a state change occurred
+     */
+    replace(newState, dontRecordStateEvents) {
+        if (this._currentStateEquals(newState)) {
+            return false;
+        }
+
+        const previousState = this._replaceImpl(newState);
+
+        if (dontRecordStateEvents) {
+            return true;
+        }
+
+        const stateChanges = this._stateChangesOf(previousState, newState);
+
+        return this.collectStateChanges(stateChanges);
+    }
+
+    /**
+     * @param {T=} previousState
+     * @param {T=} newState
+     * @return {StateChange<T>[]}
+     * @protected
+     */
+    _stateChangesOf(previousState, newState) {
+        return [new StateChange(previousState, newState)];
+    }
+
+    /**
+     * @param {T} [state] is the new state value to store
+     * @return {T} the previous state
+     * @protected
+     */
+    _replaceImpl(state) {
+        const previousState = this._currentState;
+        this._currentState = state;
+        return previousState;
+    }
+
+    /**
+     * @param {T|T=} anotherState
+     * @return {boolean}
+     * @protected
+     */
+    _currentStateEquals(anotherState) {
+        return this._currentState == null && anotherState == null || this._currentState === anotherState;
+    }
+
+    /**
+     * @return {StateChange<T>[]|undefined}
+     */
+    collectStateChanges(stateChanges) {
+        return stateChanges.map(sc => this.collectStateChange(sc));
+    }
+
+    /**
+     * @return {StateChange<T>|undefined}
+     */
+    collectStateChange(stateChange) {
+        return this._stateChanges.collect(stateChange);
+    }
+
+    reset() {
+        this._stateChanges.reset();
+        this._currentState = undefined;
     }
 }
