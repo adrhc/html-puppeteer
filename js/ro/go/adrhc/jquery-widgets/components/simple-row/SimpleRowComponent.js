c4db4aa1a9f2b387fd1de47e428a4253a4a4a199
Adrian Petre <adrian.petre@endava.com>
2021-09-01 22:07:10 +0300

    DRAFT of*() to constructors and CrudListState.js type check SimpleRowComponent.js: added state field

--- js/ro/go/adrhc/jquery-widgets/components/simple-row/SimpleRowComponent.js
+++ js/ro/go/adrhc/jquery-widgets/components/simple-row/SimpleRowComponent.js
@@ -3,6 +3,10 @@
  */
 class SimpleRowComponent extends AbstractComponent {
     /**
+     * @type {TaggingStateHolder}
+     */
+    state;
+    /**
      * @type {SimpleRowView}
      */
     view;
@@ -19,6 +23,7 @@ class SimpleRowComponent extends AbstractComponent {
      */
     constructor(options) {
         super({clearChildrenOnReset: true, dontAutoInitialize: true, ...options});
+        ObjectUtils.copyDeclaredProperties(this, this.aggregatedOptions);
         const mustacheTableElemAdapter = this.aggregatedOptions.mustacheTableElemAdapter
             ?? new MustacheTableElemAdapter(this.aggregatedOptions);
         this.view = this.aggregatedOptions.view ?? new SimpleRowView(mustacheTableElemAdapter);
