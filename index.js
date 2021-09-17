b08301a0c698c7934acd3642641003e3bbe68f8e
Adrian Petre <adrian.petre@endava.com>
2021-09-17 23:35:19 +0300

    index.html

--- index.js
+++
@@ -1,7 +0,0 @@
-import SimpleComponent from "./ro/go/adrhc/html-puppeteer/core/SimpleComponent.js";
-
-$(() => {
-    // $("#component").text("Hello puppeteer!");
-    const component = new SimpleComponent({elemIdOrJQuery: "component"});
-    component.render({text: "Hello puppeteer!"});
-})
