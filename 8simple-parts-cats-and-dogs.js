ca6002dab321741a3d639d00c10dc43aca020c57
Adrian Petre <adrian.petre@endava.com>
2021-09-21 12:44:39 +0300

    8simple-parts-cats-and-dogs.html DRAFT

---
+++ 8simple-parts-cats-and-dogs.js
@@ -0,0 +1,24 @@
+import PUPPETEER from "./ro/go/adrhc/html-puppeteer/core/Puppeteer.js";
+import COMPONENTS_FACTORY from "./ro/go/adrhc/html-puppeteer/core/ComponentsFactories.js";
+import PeriodicallyStateChangingComponent
+    from "./ro/go/adrhc/app/components/periodically-state-changing/PeriodicallyStateChangingComponent.js";
+import {generateDogsOrCats} from "./ro/go/adrhc/app/Generators.js";
+import Scenario6EventsBinder from "./ro/go/adrhc/app/scenarios/6puppeteer+state-button/Scenario6EventsBinder.js";
+import {addClockDebugger} from "./ro/go/adrhc/app/components/periodically-state-changing/PeriodicallyStateChangingOptionsBuilder.js";
+
+$(() => {
+    COMPONENTS_FACTORY.registerComponentType("periodically-state-changing", (options) => new PeriodicallyStateChangingComponent(options));
+    PUPPETEER.animate(addClockDebugger({debuggerElemIdOrJQuery: "internal-clock-debugger"})
+        .doPeriodicallyWithState(stateHolder => {
+            if (stateHolder.currentState == null) {
+                stateHolder.replace({interval: stateHolder.config.interval});
+            } else {
+                const part = generateDogsOrCats(3);
+                stateHolder.replacePart(part.value, part.name);
+            }
+        })
+        .addDebugger({debuggerElemIdOrJQuery: "component-debugger"})
+        .to({
+            eventsBinder: new Scenario6EventsBinder()
+        }));
+});
