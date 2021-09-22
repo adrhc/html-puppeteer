1e9ac3eb90258d169720422507cf0bbee13159be
Adrian Petre <adrian.petre@endava.com>
2021-09-22 10:08:13 +0300

    4puppeteer-sc-table.html: debugger textarea

--- 4puppeteer-sc-table.js
+++ 4puppeteer-sc-table.js
@@ -1,6 +1,9 @@
 import animate from "./ro/go/adrhc/html-puppeteer/core/Puppeteer.js";
 import {withDebugger} from "./ro/go/adrhc/html-puppeteer/core/component/options/DebuggerOptionsBuilder.js";
+import {getTotalHeight} from "./ro/go/adrhc/html-puppeteer/util/DomUtils.js";
 
 $(() => {
     animate(withDebugger());
+
+    $('textarea').height(getTotalHeight);
 })
