c576dcee9d3fbc7fe4908880ad8942192844a707
Adrian Petre <adrian.petre@endava.com>
2021-09-22 10:10:24 +0300

    3simpleComponent-table.html: debugger textarea

--- 3simpleComponent-table.js
+++ 3simpleComponent-table.js
@@ -1,6 +1,9 @@
 import {addDebugger} from "./ro/go/adrhc/html-puppeteer/core/component/options/DebuggerOptionsBuilder.js";
 import SimpleComponent from "./ro/go/adrhc/html-puppeteer/core/component/SimpleComponent.js";
+import {getTotalHeight} from "./ro/go/adrhc/html-puppeteer/util/DomUtils.js";
 
 $(() => {
     new SimpleComponent(addDebugger().to({elemIdOrJQuery: "component"})).render();
+
+    $('textarea').height(getTotalHeight);
 })
