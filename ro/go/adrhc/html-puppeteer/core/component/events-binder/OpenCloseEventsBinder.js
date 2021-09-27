15cfd4b26d7342fb7309d81e54521b63107a16eb
Adrian Petre <adrian.petre@endava.com>
2021-09-27 22:07:45 +0300

    OpenCloseEventsBinder.js

--- ro/go/adrhc/html-puppeteer/core/component/events-binder/OpenCloseEventsBinder.js
+++ ro/go/adrhc/html-puppeteer/core/component/events-binder/OpenCloseEventsBinder.js
@@ -9,13 +9,13 @@ export default class OpenCloseEventsBinder extends EventsBinder {
         this._attachChildEventsHandler("open", () => {
             this._$childrenHavingDataAttr("open").addClass("disabled");
             this._$childrenHavingDataAttr("close").removeClass("disabled");
-            this.component.render();
+            this._component.render();
         }, true);
         // <button data-close="click" data-owner="componentId">Close</button>
         this._attachChildEventsHandler("close", () => {
             this._$childrenHavingDataAttr("close").addClass("disabled");
             this._$childrenHavingDataAttr("open").removeClass("disabled");
-            this.component.close();
+            this._component.close();
         }, true);
     }
 }
