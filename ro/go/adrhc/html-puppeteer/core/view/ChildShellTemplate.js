2c5703779064bf8d2e29b5f743caefa58ea84345
Adrian Petre <adrian.petre@endava.com>
2021-09-29 10:48:14 +0300

    ChildShellTemplate.js

---
+++ ro/go/adrhc/html-puppeteer/core/view/ChildShellTemplate.js
@@ -0,0 +1,51 @@
+import {contentOfElemId, dataAttributesOf} from "../../util/DomUtils.js";
+import GlobalConfig, {dataComponentIdOf, dataOwnerOf, dataPartOf, dataTypeOf} from "../../util/GlobalConfig.js";
+
+/**
+ * @typedef {Object} ChildShellTemplateOptions
+ * @property {string=} shellTemplate is the shell's HTML containing the data-type and data-part
+ * @property {string=} shellTemplateId
+ * @property {string=} shellOccupantTemplateId is a shortcut for SeatAttributes.templateId
+ * @property {string=} shellOccupantHtmlTag is a shortcut for SeatAttributes.htmlTag
+ * @property {SeatAttributes=} childSeatAttributes
+ */
+/**
+ * @param {string} parentId
+ * @param {ChildShellTemplateOptions} options
+ */
+export default function shellTemplateOf(parentId, {
+    shellTemplate,
+    shellTemplateId,
+    shellOccupantTemplateId,
+    shellOccupantHtmlTag,
+    childSeatAttributes = {
+        templateId: shellOccupantTemplateId,
+        htmlTag: shellOccupantHtmlTag
+    }
+}) {
+    return shellTemplate ?? createSeatTemplate(parentId, shellTemplateId, childSeatAttributes);
+}
+
+/**
+ * @param {string} parentId
+ * @param {string=} shellTemplateId
+ * @param {SeatAttributes=} childSeatAttributes
+ * @protected
+ */
+function createSeatTemplate(parentId, shellTemplateId, childSeatAttributes) {
+    if (shellTemplateId) {
+        return contentOfElemId(shellTemplateId);
+    } else {
+        return templateFromSeatAttributes(parentId, childSeatAttributes);
+    }
+}
+
+/**
+ * @param {SeatAttributes=} childSeatAttributes
+ * @param {string} parentId
+ * @return {string}
+ * @protected
+ */
+function templateFromSeatAttributes(parentId, {templateId, htmlTag = "div", componentType = "simple", ...rest}) {
+    return `<${htmlTag} ${dataTypeOf(componentType)} ${dataOwnerOf(parentId)} ${dataPartOf(`{{${GlobalConfig.PART}}}`)} ${dataComponentIdOf(`{{${GlobalConfig.COMPONENT_ID}}}`)} data-template-id="${templateId}" ${dataAttributesOf(rest)}></${htmlTag}>`;
+}
--- ro/go/adrhc/html-puppeteer/core/view/ChildrenShellsView.js
+++ ro/go/adrhc/html-puppeteer/core/view/ChildrenShellsView.js
@@ -1,9 +1,10 @@
 import AbstractView from "./AbstractView.js";
-import {contentOfElemId, dataAttributesOf, jQueryOf} from "../../util/DomUtils.js";
-import GlobalConfig, {dataComponentIdOf, dataOwnerOf, dataPartOf, dataTypeOf} from "../../util/GlobalConfig.js";
+import {jQueryOf} from "../../util/DomUtils.js";
+import GlobalConfig from "../../util/GlobalConfig.js";
 import {dataPartSelectorOf} from "../../util/SelectorUtils.js";
 import {generateHtml} from "../../util/HtmlGenerator.js";
 import {uniqueId} from "../../util/StringUtils.js";
+import shellTemplateOf from "./ChildShellTemplate.js";
 
 /**
  * @typedef {Bag} SeatAttributes
@@ -12,16 +13,10 @@ import {uniqueId} from "../../util/StringUtils.js";
  * @property {string=} htmlTag
  */
 /**
- * @typedef {Object} GuestsRoomViewOptions
+ * @typedef {ChildShellTemplateOptions} GuestsRoomViewOptions
  * @property {string} componentId
  * @property {string|jQuery<HTMLElement>=} elemIdOrJQuery is the parent's element id or jQuery<HTMLElement>
- * @property {string=} shellTemplate is the shell's HTML containing the data-type and data-part
- * @property {string=} shellTemplateId
- * @property {string=} shellOccupantTemplateId is a shortcut for SeatAttributes.templateId
- * @property {string=} shellOccupantHtmlTag is a shortcut for SeatAttributes.htmlTag
  * @property {boolean=} newGuestsGoLast
- * @property {boolean=} dontRemoveGuests
- * @property {SeatAttributes=} childSeatAttributes
  */
 /**
  * @extends {AbstractView}
@@ -50,22 +45,19 @@ export default class ChildrenShellsView extends AbstractView {
 
     /**
      * @param {GuestsRoomViewOptions} options
+     * @param {ChildShellTemplateOptions} options.restOfOptions
      */
     constructor({
                     componentId,
                     elemIdOrJQuery,
                     newGuestsGoLast,
-                    shellTemplate,
-                    shellTemplateId,
-                    shellOccupantTemplateId,
-                    shellOccupantHtmlTag,
-                    childSeatAttributes = {templateId: shellOccupantTemplateId, htmlTag: shellOccupantHtmlTag}
+                    ...restOfOptions
                 }) {
         super();
         this.parentId = componentId;
         this.$containerElem = jQueryOf(elemIdOrJQuery);
         this.place = (newGuestsGoLast ?? false) ? "append" : "prepend";
-        this.shellTemplate = shellTemplate ?? this._createSeatTemplate(shellTemplateId, childSeatAttributes);
+        this.shellTemplate = shellTemplateOf(componentId, restOfOptions);
     }
 
     /**
@@ -98,27 +90,5 @@ export default class ChildrenShellsView extends AbstractView {
      */
     _childSeatExists(partName) {
         return !!this.$containerElem.children(dataPartSelectorOf(name)).length;
-    }
-
-    /**
-     * @param {string=} shellTemplateId
-     * @param {SeatAttributes=} childSeatAttributes
-     * @protected
-     */
-    _createSeatTemplate(shellTemplateId, childSeatAttributes) {
-        if (shellTemplateId) {
-            return contentOfElemId(shellTemplateId);
-        } else {
-            return this._templateFromSeatAttributes(childSeatAttributes);
-        }
-    }
-
-    /**
-     * @param {SeatAttributes=} childSeatAttributes
-     * @return {string}
-     * @protected
-     */
-    _templateFromSeatAttributes({templateId, htmlTag = "div", componentType = "simple", ...rest}) {
-        return `<${htmlTag} ${dataTypeOf(componentType)} ${dataOwnerOf(this.parentId)} ${dataPartOf(`{{${GlobalConfig.PART}}}`)} ${dataComponentIdOf(`{{${GlobalConfig.COMPONENT_ID}}}`)} data-template-id="${templateId}" ${dataAttributesOf(rest)}></${htmlTag}>`;
     }
 }
