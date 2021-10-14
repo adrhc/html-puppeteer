09c0bd970f172afda75a47409c6e9d3a3c660551
Adrian Petre <adrian.petre@endava.com>
2021-10-14 14:58:39 +0300

    DynamicContainerComponent.constructor comment

--- ro/go/adrhc/html-puppeteer/core/component/DynamicContainerComponent.js
+++ ro/go/adrhc/html-puppeteer/core/component/DynamicContainerComponent.js
@@ -1,13 +1,12 @@
 import SimpleContainerComponent from "./SimpleContainerComponent.js";
 import {jQueryOf} from "../../util/DomUtils.js";
 
-/**
- * ChildrenShells have the chance to use parentHtml for children shells.
- * We have to set htmlTemplate to "" to avoid the drawing of the parentHtml
- * which now is a candidate to be children shell template.
- */
 export default class DynamicContainerComponent extends SimpleContainerComponent {
     /**
+     * ChildrenShells have the chance to use parentHtml for children shells.
+     * We have to set htmlTemplate to "" to avoid the drawing of the parentHtml
+     * which now is a candidate to be children shell template.
+     *
      * @param {SimpleContainerComponentOptions} options
      */
     constructor(options) {
