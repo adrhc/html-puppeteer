d3206e8e58febb0b1b30a7397d159d555f039b35
Adrian Petre <adrian.petre@endava.com>
2021-11-11 20:02:46 +0200

    15dynamic-list.js using Utils.js

--- 15dynamic-list.js
+++ 15dynamic-list.js
@@ -1,13 +1,12 @@
 import animate from "./ro/go/adrhc/html-puppeteer/core/Puppeteer.js";
-import OpenCloseEventsBinder from "./ro/go/adrhc/app/components/event-binders/OpenCloseEventsBinder.js";
 import {generateString} from "./ro/go/adrhc/app/Generators.js";
-import StateChangeEventsBinder from "./ro/go/adrhc/app/components/event-binders/StateChangeEventsBinder.js";
-import {addDebugger} from "./ro/go/adrhc/html-puppeteer/core/component/options/DebuggerOptionsBuilder.js";
+import {debuggingOptionsOf} from "./ro/go/adrhc/app/util/Utils.js";
+import {withDefaults} from "./ro/go/adrhc/html-puppeteer/core/component/options/ComponentOptionsBuilder.js";
 
 $(() => {
-    animate(addDebugger({elemIdOrJQuery: "debugger-component"})
-        .addEventsBinders(new StateChangeEventsBinder("debugger-component"), new OpenCloseEventsBinder())
-        .to({
-            childStateProviderFn: () => ({id: Math.random(), name: generateString("name ")})
-        }));
+    const debuggingOptions = debuggingOptionsOf("MAIN-debugger")
+    const mainOptions = withDefaults(debuggingOptions).to({
+        childStateProviderFn: () => ({id: Math.random(), name: generateString("name ")})
+    });
+    animate({MAIN: mainOptions});
 });
