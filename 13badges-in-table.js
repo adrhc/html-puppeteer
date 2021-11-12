c9f042bb211217e903646c6b8d0cbfe8281a3356
Adrian Petre <adrian.petre@endava.com>
2021-11-12 08:22:53 +0200

    13badges-in-table.js using commonOptionsOf

--- 13badges-in-table.js
+++ 13badges-in-table.js
@@ -1,12 +1,12 @@
-import {addDebugger} from "./ro/go/adrhc/html-puppeteer/core/component/options/DebuggerOptionsBuilder.js";
 import animate from "./ro/go/adrhc/html-puppeteer/core/Puppeteer.js";
 import CatsWithKittensEventsBinder from "./ro/go/adrhc/app/components/event-binders/CatsWithKittensEventsBinder.js";
-import StateChangeEventsBinder from "./ro/go/adrhc/app/components/event-binders/StateChangeEventsBinder.js";
+import {commonOptionsOf} from "./ro/go/adrhc/app/util/Utils.js";
+import {withDefaults} from "./ro/go/adrhc/html-puppeteer/core/component/options/ComponentOptionsBuilder.js";
 
 $(() => {
-    const eventsBinders = [new StateChangeEventsBinder("MAIN-debugger"),
-        component => new CatsWithKittensEventsBinder(component)];
-    const options = addDebugger({elemIdOrJQuery: "MAIN-debugger"})
-        .addEventsBinders(...eventsBinders).options();
-    animate(options);
+    const debuggingOptions = commonOptionsOf("MAIN-debugger")
+    const mainOptions = withDefaults(debuggingOptions)
+        .addEventsBinders(component => new CatsWithKittensEventsBinder(component))
+        .options();
+    animate({MAIN: mainOptions});
 });
