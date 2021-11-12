2c08aad998a9f350aff227459cecd8f4aad3c733
Adrian Petre <adrian.petre@endava.com>
2021-11-12 08:57:42 +0200

    10div-list-default-shell.html: debugger and controls moved to a static component

--- 10div-list-default-shell.js
+++ 10div-list-default-shell.js
@@ -1,12 +1,12 @@
-import {addDebugger} from "./ro/go/adrhc/html-puppeteer/core/component/options/DebuggerOptionsBuilder.js";
 import animate from "./ro/go/adrhc/html-puppeteer/core/Puppeteer.js";
 import CatsAndDogsEventsBinder from "./ro/go/adrhc/app/components/event-binders/CatsAndDogsEventsBinder.js";
-import StateChangeEventsBinder from "./ro/go/adrhc/app/components/event-binders/StateChangeEventsBinder.js";
+import {commonOptionsOf} from "./ro/go/adrhc/app/util/Utils.js";
+import {withDefaults} from "./ro/go/adrhc/html-puppeteer/core/component/options/ComponentOptionsBuilder.js";
 
 $(() => {
-    const eventsBinders = [new StateChangeEventsBinder("MAIN-debugger"),
-        component => new CatsAndDogsEventsBinder(component)];
-    const options = addDebugger({elemIdOrJQuery: "MAIN-debugger"})
-        .addEventsBinders(...eventsBinders).options();
-    animate(options);
+    const commonOptions = commonOptionsOf("MAIN-debugger");
+    const mainOptions = withDefaults(commonOptions)
+        .addEventsBinders(component => new CatsAndDogsEventsBinder(component))
+        .options();
+    animate({MAIN: mainOptions});
 });
