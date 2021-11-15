d35b954fe363b05e30b674df67eb7e8053230f2b
Adrian Petre <adrian.petre@endava.com>
2021-11-15 18:47:27 +0200

    SwitcherComponent.js: fixed activeName usage

--- ro/go/adrhc/html-puppeteer/core/component/SwitcherComponent.js
+++ ro/go/adrhc/html-puppeteer/core/component/SwitcherComponent.js
@@ -128,11 +128,7 @@ export default class SwitcherComponent extends AbstractContainerComponent {
     _switchUsingCurrentState() {
         const stateCopy = this.getStateCopy();
         const activeName = stateCopy?.[this.activeNameKey];
-        if (typeof activeName === "string") {
-            this._switchTo(activeName, this._getActiveValueFromState(stateCopy));
-        } else if (activeName != null) {
-            activeName.forEach(name => this._switchTo(name, this._getActiveValueFromState(stateCopy)))
-        }
+        this._switchTo(activeName, this._getActiveValueFromState(stateCopy));
     }
 
     /**
